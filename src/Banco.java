effd80813aea4944c857dbffdceaa9912a7238ce
Sterben22 <sterben97061@gmail.com>
2023-09-04 23:35:15 -0500

    firts commit

---
+++ src/Banco.java
@@ -0,0 +1,29 @@
+class CuentaBAncaria{
+    int saldo;
+    String num_cuenta;
+    CuentaBAncaria(String num_cuenta, int saldo){
+        this.num_cuenta = num_cuenta;
+        this.saldo = saldo;
+    }
+    public void depositar (int depos){
+        this.saldo += depos;
+    }
+
+    public void retirar (int retir){
+        this.saldo -= retir;
+
+    }
+}
+
+public class Banco {
+    public static void main(String[] args){
+        CuentaBAncaria person = new CuentaBAncaria("12345",300);
+
+        System.out.printf("Saldo actual es %d \n",person.saldo);
+        person.depositar(30);
+        System.out.printf("SAldo actual es %d \n",person.saldo);
+        person.retirar(15);
+        System.out.printf("SAldo actual es %d \n", person.saldo);
+
+    }
+}
---
+++ src/Prac_1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Person {
+    String name, age, profession;
+
+    public Person(String name, String age, String profession){
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+}
+
+public class Prac_1 {
+    public static void print_line(Person person){
+        System.out.println("###########");
+        System.out.printf("Nombre: %s \n Edad: %s \n Profesion: %s \n", person.name,person.age, person.profession);
+        System.out.println("###########");
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String name,profession, age;
+        System.out.println("Cuantas personas ingresara: ");
+        int n = scan.nextInt();
+        scan.nextLine();
+        Person[] list_Person = new Person[n];
+
+        for(int i=0; i<n; i++){
+            System.out.printf("Ingrese el nombre de la Persona %d: ", i+1);
+            name = scan.nextLine();
+            System.out.printf("Ingrese la edad de la Persona %d: ", i+1);
+            age = scan.nextLine();
+            System.out.printf("Ingrese la profesion de la Persona %d: ", i+1);
+            profession = scan.nextLine();
+
+            list_Person[i] = new Person(name,age,profession);
+        }
+        for(int i=0; i<n; i++){
+            print_line(list_Person[i]);
+        }
+    }
+}
