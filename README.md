fba32c6b57babaf497c100ebc5c1367440680be9
Sterben22 <sterben97061@gmail.com>
2023-09-10 21:04:51 -0500

    xd

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Year
-## Hola Sterben
+- Estructura de Datos
+- Estudiante: Diego A. Espinoza H.
+
+# Información
+Se estara subiendo los codigos del curso EdD.
--- src/Encap_Const.java
+++ src/Encap_Const.java
@@ -1,29 +1,58 @@
 class CuentaBAncaria{
-    int saldo;
-    String num_cuenta;
+    private int saldo;
+    private String num_cuenta;
     CuentaBAncaria(String num_cuenta, int saldo){
         this.num_cuenta = num_cuenta;
         this.saldo = saldo;
     }
-    public void depositar (int depos){
-        this.saldo += depos;
+
+    public int getSaldo() {
+        return saldo;
     }
 
-    public void retirar (int retir){
-        this.saldo -= retir;
+    public String getNum_cuenta() {
+        return num_cuenta;
+    }
 
+    public void setNum_cuenta(String num_cuenta) {
+        this.num_cuenta = num_cuenta;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar (int monto) throws Exception{
+        if(monto >0){
+            this.saldo += monto;
+        }else {
+            throw new Exception("Monto a depositar no valido");
+        }
+    }
+
+    public void retirar (int monto) throws Exception{
+        if(monto > 0 && monto <= this.saldo){
+            this.saldo -= monto;
+        }else {
+            throw new Exception("Monto a retirar no valido");
+        }
     }
 }
 
 public class Encap_Const {
     public static void main(String[] args){
-        CuentaBAncaria person = new CuentaBAncaria("12345",300);
+        CuentaBAncaria account = new CuentaBAncaria("12345",300);
 
-        System.out.printf("Saldo actual es %d \n",person.saldo);
-        person.depositar(30);
-        System.out.printf("SAldo actual es %d \n",person.saldo);
-        person.retirar(15);
-        System.out.printf("SAldo actual es %d \n", person.saldo);
+        try {
+            System.out.printf("Saldo actual es %d \n",account.getSaldo());
+            account.depositar(-30);
+            System.out.printf("Saldo actual es %d \n",account.getSaldo());
+            account.retirar(35);
+            System.out.printf("Saldo actual es %d \n", account.getSaldo());
+        }catch (Exception err){
+            System.out.println(err.getMessage());
+        }
+
 
     }
 }
